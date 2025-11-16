e8985f32d88b9b418cf8d318045f005413ed1957
saifulAlam <gmsaiful71@gmail.com>
2025-11-16 12:52:04 +0600

    first commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\ApiController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/register', [ApiController::class, 'register']);
+Route::post('/login', [ApiController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiresource('/posts', PostController::class);
+    Route::post('/logout', [ApiController::class, 'logout']);
+});
+
