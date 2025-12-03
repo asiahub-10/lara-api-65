0a5998f296b3ee43afbac742399424916176d6eb
saifulAlam <gmsaiful71@gmail.com>
2025-12-03 13:12:12 +0600

    api

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        return response()->json([
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $category = Category::create($request->all());
+
+        return response()->json([
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $category = Category::update($request->all());
+
+        return response()->json([
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -16,4 +18,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiresource('/posts', PostController::class);
     Route::post('/logout', [ApiController::class, 'logout']);
 });
+
+Route::apiresource('/categories', CategoryController::class);
+Route::apiresource('/products', ProductController::class);
 
